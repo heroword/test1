c0e43365fd79742505de5fa5c1c625d15004d629
BaohuaSong <dlt_songxiaobao@163.com>
2016-05-30 18:57:15 +0800

    hhhh

--- testproject/Form1.cs
+++ testproject/Form1.cs
@@ -23,7 +23,7 @@ namespace testproject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.button1.Text = "ee";
+            this.button1.Text = "efe";
         }
     }
 }
