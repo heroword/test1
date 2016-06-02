6c585e63cd81507ebd9fbca3c1f6090b0633c521
Kadail Song <dlt_songxiaobao@163.com>
2016-06-02 13:27:09 +0800

    Update Form1.cs

--- testproject/Form1.cs
+++ testproject/Form1.cs
@@ -23,7 +23,7 @@ namespace testproject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.button1.Text = "efe";
+            this.button1.Text = "efttte";
         }
     }
 }
